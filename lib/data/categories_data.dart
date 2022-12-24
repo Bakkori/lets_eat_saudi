c9fbb281da36faa406f7875eac2870a54433cd6b
Bakkori <bakkori990@gmail.com>
2022-12-24 21:10:55 +0300

    create and set categories objects

---
+++ lib/data/categories_data.dart
@@ -0,0 +1,14 @@
+import 'package:lets_eat_saudi/models/category.dart';
+
+class CategoriesData {
+  get categoriesData {
+    return [
+      Category(101, 'حجازي'),
+      Category(102, 'نجدي'),
+      Category(103, 'قصيمي'),
+      Category(104, 'شرقي'),
+      Category(105, 'شمالي'),
+      Category(106, 'جنوبي'),
+    ];
+  }
+}
