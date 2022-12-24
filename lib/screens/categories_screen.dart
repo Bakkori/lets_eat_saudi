5b500427a127b2b13f379f1d12c1502c05994476
Bakkori <bakkori990@gmail.com>
2022-12-24 21:11:25 +0300

    Create categories_screen.dart

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:lets_eat_saudi/data/categories_data.dart';
+import 'package:lets_eat_saudi/widgets/category_item.dart';
+
+class CategoriesScreen extends StatefulWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  List categories = CategoriesData().categoriesData;
+
+  List<CategoryItem> categoriesWidgetList = [];
+  setCategoriesWidgetList() {
+    for (var i = 0; i < categories.length; i++) {
+      categoriesWidgetList
+          .add(CategoryItem(id: categories[i].id, name: categories[i].name));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setCategoriesWidgetList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          //  centerTitle: true,
+          title: const Text('التصنيفات'),
+          // backgroundColor: backgroundColor,
+          // elevation: 0,
+        ),
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 255 / 431,
+          crossAxisSpacing: 15,
+          padding: const EdgeInsets.all(15),
+          children: [...categoriesWidgetList],
+        ));
+  }
+}
