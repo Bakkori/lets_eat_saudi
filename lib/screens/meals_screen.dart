fa0ec489bbb2c605e143c1d5f56ebb8f5b705bb4
Bakkori <bakkori990@gmail.com>
2022-12-26 17:36:52 +0300

    Create meals_screen.dart

---
+++ lib/screens/meals_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:lets_eat_saudi/data/meals_data.dart';
+import 'package:lets_eat_saudi/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TO DO: create a function to make a list of meals for a specific category
+    List mealsList = MealsData().mealsData;
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(
+        title: const Text('الأطباق'),
+      ),
+      body: GridView.builder(
+          itemCount: mealsList.length,
+          padding: const EdgeInsets.all(15),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemBuilder: (ctx, index) {
+            return MealItem(
+                imageUrl: mealsList[index].imageUrl,
+                name: mealsList[index].name);
+          }),
+    );
+  }
+}
