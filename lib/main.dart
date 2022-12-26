de027335a375718a6fc6236de88a4ddfdc9c088d
Bakkori <bakkori990@gmail.com>
2022-12-26 17:35:07 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lets_eat_saudi/screens/categories_screen.dart';
+import 'package:lets_eat_saudi/screens/meals_screen.dart';
 import 'package:lets_eat_saudi/widgets/category_item.dart';
 import 'package:lets_eat_saudi/widgets/meal_item.dart';
 
@@ -15,20 +16,20 @@ class LetsEatSaudi extends StatelessWidget {
     const backgroundColor = Color.fromRGBO(214, 214, 187, 1);
     const secondaryColor = Color.fromRGBO(149, 39, 57, 1);
     return MaterialApp(
-      checkerboardOffscreenLayers: true,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          backgroundColor: backgroundColor,
-          appBarTheme: const AppBarTheme(
-            centerTitle: true,
+        checkerboardOffscreenLayers: true,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
             backgroundColor: backgroundColor,
-            elevation: 0,
-            titleTextStyle: TextStyle(
-                color: secondaryColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 20),
-          )),
-      home: const CategoriesScreen(),
-    );
+            appBarTheme: const AppBarTheme(
+              foregroundColor: secondaryColor,
+              centerTitle: true,
+              backgroundColor: backgroundColor,
+              elevation: 0,
+              titleTextStyle: TextStyle(
+                  color: secondaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            )),
+        home: CategoriesScreen());
   }
 }
