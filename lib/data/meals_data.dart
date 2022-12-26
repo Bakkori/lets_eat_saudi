919e1d1725f427a483aa4651e4c2dccde305c015
Bakkori <bakkori990@gmail.com>
2022-12-26 17:34:37 +0300

    add some meals

---
+++ lib/data/meals_data.dart
@@ -0,0 +1,45 @@
+import 'package:lets_eat_saudi/models/meals.dart';
+
+class MealsData {
+  get mealsData {
+    return [
+      Meal(
+          1011,
+          'المعصوب',
+          [101],
+          true,
+          true,
+          false,
+          false,
+          false,
+          true,
+          ['موز', 'فطيرة'],
+          ['عسل', 'قشطة', 'سمن', 'حليب مبخر'],
+          'https://menu360.me/wp-content/uploads/2021/08/0Y2A0110.jpg',
+          ''),
+      Meal(
+          1012,
+          'السليق الطائفي',
+          [101],
+          false,
+          false,
+          true,
+          true,
+          false,
+          false,
+          [
+            'دجاج أو لحم',
+            'ارز مصري',
+            'فص مستكة',
+            'حبوب هيل',
+            'بصلة',
+            'فلفل أبيض',
+            'زبدة',
+            'حليب سائل'
+          ],
+          [],
+          'https://cdn.alweb.com/thumbs/recipes/article/fit727x484/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9-%D8%B9%D9%85%D9%84-%D8%A7%D9%84%D8%B3%D9%84%D9%8A%D9%82-%D8%A7%D9%84%D8%B7%D8%A7%D8%A6%D9%81%D9%8A.jpg',
+          'https://cookpad.com/sa/وصفات/504279-طريقة-عمل-السليق-الطائفي'),
+    ];
+  }
+}
