130025a4e55754ed4fd23031fdd5e0902b163853
Bakkori <bakkori990@gmail.com>
2022-12-28 10:46:58 +0300

    Add a method to get meals based on category

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,31 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:lets_eat_saudi/data/meals_data.dart';
 import 'package:lets_eat_saudi/widgets/meal_item.dart';
+import '../models/meals.dart';
 
-class MealsScreen extends StatelessWidget {
+class MealsScreen extends StatefulWidget {
   const MealsScreen({Key? key, required this.category_id}) : super(key: key);
 
   final int category_id;
 
   @override
+  State<MealsScreen> createState() => _MealsScreenState();
+}
+
+class _MealsScreenState extends State<MealsScreen> {
+  List mealsList = MealsData().mealsData;
+  List meals = [];
+
+  @override
+  void initState() {
+    // there will be list of categories, make a method to get the desired meals
+    // based on the category.
+    for (var i = 0; i < mealsList.length; i++) {
+      for (var j = 0; j < mealsList[i].categories.length; j++) {
+        if (mealsList[i].categories[j] == widget.category_id) {
+          meals.add(mealsList[i]);
+        }
+      }
+    }
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     // TO DO: create a function to make a list of meals for a specific category
-    List mealsList = MealsData().mealsData;
+
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
         title: const Text('الأطباق'),
       ),
       body: GridView.builder(
-          itemCount: mealsList.length,
+          itemCount: meals.length,
           padding: const EdgeInsets.all(15),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
           ),
           itemBuilder: (ctx, index) {
             return MealItem(
-                imageUrl: mealsList[index].imageUrl,
-                name: mealsList[index].name);
+                imageUrl: meals[index].imageUrl, name: meals[index].name);
           }),
     );
   }
