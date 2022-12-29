47ba5b6b0ffff03a55ce1c47eba9bb631610358d
Bakkori <bakkori990@gmail.com>
2022-12-29 10:15:44 +0300

    Some update on data

--- lib/data/meals_data.dart
+++ lib/data/meals_data.dart
@@ -37,7 +37,7 @@ class MealsData {
             'زبدة',
             'حليب سائل'
           ],
-          [],
+          ['السمن البري'],
           'https://cdn.alweb.com/thumbs/recipes/article/fit727x484/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9-%D8%B9%D9%85%D9%84-%D8%A7%D9%84%D8%B3%D9%84%D9%8A%D9%82-%D8%A7%D9%84%D8%B7%D8%A7%D8%A6%D9%81%D9%8A.jpg',
           'https://cookpad.com/sa/وصفات/504279-طريقة-عمل-السليق-الطائفي'),
     ];
