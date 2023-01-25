62c7c608e48fdbf5ab593069e91406198e13c072
Bakkori <bakkori990@gmail.com>
2023-01-25 17:53:12 +0300

    Update reviews with provider

--- lib/models/data/reviews.dart
+++ lib/models/data/reviews.dart
@@ -1,16 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:lets_eat_saudi/models/review.dart';
 
-class Reviews {
-  List<Review> reviews = [
+class Reviews with ChangeNotifier {
+  List<Review> _reviews = [
     Review('من أفضل الأكلات الي جربتها', 1011),
+    Review('روعة مع القشطة', 1011),
+    Review('أكلة لا توصف', 1012),
   ];
+  List<Review> get reviews {
+    // check the ...
+    return [..._reviews];
+  }
 
-  getReviews() {
-    return reviews;
+  List<Review> getReviews(int mealId) {
+    return _reviews.where((rev) => mealId == rev.mealId).toList();
   }
 
 // use saved perfences
   addReviw(String review, int mealId) {
-    reviews.add(Review(review, mealId));
+    _reviews.add(Review(review, mealId));
+    notifyListeners();
+  }
+
+  printRev() {
+    for (var i = 0; i < _reviews.length; i++) {
+      print(_reviews[i].review);
+    }
   }
 }
