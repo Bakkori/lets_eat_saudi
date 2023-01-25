8cf9e5b26882970e6dd169867ac924e5720bc054
Bakkori <bakkori990@gmail.com>
2023-01-25 17:52:42 +0300

    Create a provider for Reviews

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lets_eat_saudi/models/data/reviews.dart';
 import 'package:lets_eat_saudi/screens/categories_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const LetsEatSaudi());
@@ -12,31 +14,34 @@ class LetsEatSaudi extends StatelessWidget {
   Widget build(BuildContext context) {
     const backgroundColor = Color.fromRGBO(214, 214, 187, 1);
     const secondaryColor = Color.fromRGBO(149, 39, 57, 1);
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Let's Eat Saudi",
-        theme: ThemeData(
-          useMaterial3: true,
-          //  androidOverscrollIndicator: AndroidOverscrollIndicator.stretch,
-          backgroundColor: backgroundColor,
-          splashColor: secondaryColor,
-          appBarTheme: const AppBarTheme(
-            foregroundColor: secondaryColor,
-            centerTitle: true,
+    return ChangeNotifierProvider(
+      create: (context) => Reviews(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "Let's Eat Saudi",
+          theme: ThemeData(
+            useMaterial3: true,
+            //  androidOverscrollIndicator: AndroidOverscrollIndicator.stretch,
             backgroundColor: backgroundColor,
-            elevation: 0,
-            titleTextStyle: TextStyle(
-                color: secondaryColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 20),
+            splashColor: secondaryColor,
+            appBarTheme: const AppBarTheme(
+              foregroundColor: secondaryColor,
+              centerTitle: true,
+              backgroundColor: backgroundColor,
+              elevation: 0,
+              titleTextStyle: TextStyle(
+                  color: secondaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+            textTheme: ThemeData.light().textTheme.copyWith(
+                bodyText1: TextStyle(
+                    color: Theme.of(context).splashColor,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500),
+                button: const TextStyle(color: backgroundColor)),
           ),
-          textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(
-                  color: Theme.of(context).splashColor,
-                  fontSize: 22,
-                  fontWeight: FontWeight.w500),
-              button: const TextStyle(color: backgroundColor)),
-        ),
-        home: const CategoriesScreen());
+          home: const CategoriesScreen()),
+    );
   }
 }
